e782809fdc7e75097c62ceebdc52651c780d1429
mehdinoori20 <mehdi.noori1994@hotmail.com>
2024-06-11 10:50:03 +0200

    first commit

---
+++ Grupparbete/src/main/java/se/mehdi/securewebapp/GrupparbeteApplication.java
@@ -0,0 +1,13 @@
+package se.mehdi.securewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupparbeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupparbeteApplication.class, args);
+	}
+
+}
---
+++ Grupparbete/src/test/java/se/mehdi/securewebapp/GrupparbeteApplicationTests.java
@@ -0,0 +1,13 @@
+package se.mehdi.securewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupparbeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
